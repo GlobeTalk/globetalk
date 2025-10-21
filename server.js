effab9091dff89eb0de52f03622668acf1497a34
Pahblo-420 <lipopopaballo022@gmail.com>
2025-10-21 17:27:27 +0200

    redeploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 const distPath = path.join(__dirname, "dist");
 app.use(express.static(distPath));
 
-app.get("/:path(*)", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 

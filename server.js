5523ef65b106e21cefc28ea176da87cb45d577ad
Pahblo-420 <lipopopaballo022@gmail.com>
2025-10-21 18:35:33 +0200

    redeploy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const distPath = path.join(__dirname, "dist");
 app.use(express.static(distPath));
 
 // Catch-all route to serve index.html for SPA routing
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(distPath, "index.html"));
 });
 
